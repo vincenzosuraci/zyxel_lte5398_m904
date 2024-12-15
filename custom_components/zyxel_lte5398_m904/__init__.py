35c4a3d794d1e44d14b7704c9a0120c1de93dbf8
Vincenzo Suraci <suraci@diag.uniroma1.it>
2024-12-15 16:39:19 +0100

    Aggiunto il pulsante di reset del dispositivo

--- custom_components/zyxel_lte5398_m904/__init__.py
+++ custom_components/zyxel_lte5398_m904/__init__.py
@@ -3,6 +3,7 @@ _LOGGER = logging.getLogger(__name__)
 
 from .const import (
     SENSOR,
+    BUTTON,
     DOMAIN,
     DEVICE_MANUFACTURER,
     CONF_IP_ADDRESS,
@@ -42,15 +43,15 @@ try:
         # Esegui il primo aggiornamento
         await coordinator.async_config_entry_first_refresh()
 
-        # Utilizza `async_add_platform` per configurare la piattaforma sensor
-        # hass.config_entries.async_setup_platforms(config_entry, [SENSOR])
-        await hass.config_entries.async_forward_entry_setups(config_entry, [SENSOR])
+        # Utilizza `async_forward_entry_setups` per configurare la piattaforma sensor e button
+        await hass.config_entries.async_forward_entry_setups(config_entry, [SENSOR, BUTTON])
 
         return True
 
 
     async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
         await hass.config_entries.async_forward_entry_unload(config_entry, SENSOR)
+        await hass.config_entries.async_forward_entry_unload(config_entry, BUTTON)
         hass.data[DOMAIN].pop(config_entry.entry_id)
 
         return True
